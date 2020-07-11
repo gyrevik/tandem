74f7116288334ace31401cbf0ec97d3d70564449
Alex Smotritsky <alexsmotritsky@MacBook-Pro.home>
2020-07-11 15:56:22 -0400

    clean up

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -12,7 +12,7 @@ function Main() {
     <div>
       <p>mean: {mean(data_1234.data)}</p>
       <p>median: {median(data_1234.data)}</p>
-      <p>standard deviation: {sd(data_1234.data)}</p>
+      <p>standard deviation: {standard_deviation(data_1234.data)}</p>
       <p>mean: {mean(data_1234.data)}</p>
     </div>
   );
@@ -21,10 +21,6 @@ function Main() {
 export default Main;
 
 // helpers
-function getExclamationMarks(numChars: number) {
-  return Array(numChars + 1).join('!');
-}
-
 function mean(arr: Array<number>) {
   return arr.reduce((accumulator, currentValue) => accumulator + currentValue) / arr.length;
 }
@@ -35,39 +31,11 @@ function median(arr: Array<number>) {
   return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
 };
 
-function standard_deviation(numChars: number) {
-  return Array(numChars + 1).join('!');
-}
-
 function mode(numChars: number) {
   return Array(numChars + 1).join('!');
 }
 
-function standardDeviation(values: Array<number>){
-  var avg = average(values);
-  
-  var squareDiffs = values.map(function(value){
-    var diff = value - avg;
-    var sqrDiff = diff * diff;
-    return sqrDiff;
-  });
-  
-  var avgSquareDiff = average(squareDiffs);
-
-  var stdDev = Math.sqrt(avgSquareDiff);
-  return stdDev;
-}
-
-function average(data: Array<number>){
-  var sum = data.reduce(function(sum, value){
-    return sum + value;
-  }, 0);
-
-  var avg = sum / data.length;
-  return avg;
-}
-
-function sd(array: Array<number>) {
+function standard_deviation(array: Array<number>) {
   var avg = _.sum(array) / array.length;
   return Math.sqrt(_.sum(_.map(array, (i) => Math.pow((i - avg), 2))) / array.length);
 };
