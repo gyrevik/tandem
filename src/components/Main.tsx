c530010c8b83f6bde3867e5f2f0508ea5556b1e3
Alex Smotritsky <alexsmotritsky@MacBook-Pro.home>
2020-07-11 16:04:58 -0400

    got mode

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -13,7 +13,7 @@ function Main() {
       <p>mean: {mean(data_1234.data)}</p>
       <p>median: {median(data_1234.data)}</p>
       <p>standard deviation: {standard_deviation(data_1234.data)}</p>
-      <p>mean: {mean(data_1234.data)}</p>
+      <p>mode: {mode(data_1234.data)}</p>
     </div>
   );
 }
@@ -31,8 +31,28 @@ function median(arr: Array<number>) {
   return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
 };
 
-function mode(numChars: number) {
-  return Array(numChars + 1).join('!');
+function mode(numbers: Array<number>) {
+  // as result can be bimodal or multi-modal,
+  // the returned result is provided as an array
+  // mode of [3, 5, 4, 4, 1, 1, 2, 3] = [1, 3, 4]
+  var modes = [], count: Array<number> = [], i, number, maxIndex = 0;
+
+  for (i = 0; i < numbers.length; i += 1) {
+      number = numbers[i];
+      count[number] = (count[number] || 0) + 1;
+      if (count[number] > maxIndex) {
+          maxIndex = count[number];
+      }
+  }
+
+  for (i in count)
+      if (count.hasOwnProperty(i)) {
+          if (count[i] === maxIndex) {
+              modes.push(Number(i));
+          }
+      }
+
+  return modes;
 }
 
 function standard_deviation(array: Array<number>) {
