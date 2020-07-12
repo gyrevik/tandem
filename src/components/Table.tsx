a5ed10d24ee7e73546144aedbc9276700b85736c
Alex Smotritsky <alexsmotritsky@MacBook-Pro.home>
2020-07-12 09:12:56 -0400

    work in progress

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -43,9 +43,6 @@ export default function SimpleTable({ mean, median, standardDeviation, mode }: S
           <TableRow>
             <TableCell>mean</TableCell>
             <TableCell align="right">{mean}</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -55,9 +52,6 @@ export default function SimpleTable({ mean, median, standardDeviation, mode }: S
                 {row.name}
               </TableCell>
               <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
             </TableRow>
           ))}
         </TableBody>
