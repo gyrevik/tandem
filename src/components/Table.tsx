ced4850b6b80c2cf164fc30ad3f8acc1bfd8c74d
Alex Smotritsky <alexsmotritsky@MacBook-Pro.home>
2020-07-12 08:46:57 -0400

    got mean into Table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -41,8 +41,8 @@ export default function SimpleTable({ mean, median, standardDeviation, mode }: S
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
+            <TableCell>mean</TableCell>
+            <TableCell align="right">{mean}</TableCell>
             <TableCell align="right">Fat&nbsp;(g)</TableCell>
             <TableCell align="right">Carbs&nbsp;(g)</TableCell>
             <TableCell align="right">Protein&nbsp;(g)</TableCell>
