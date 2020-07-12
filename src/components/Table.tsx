6cedb4ff9d68ec8c7ee627ab956d55a92d4a9ad8
Alex Smotritsky <alexsmotritsky@MacBook-Pro.home>
2020-07-12 09:17:42 -0400

    looking ok

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -14,17 +14,11 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
-  return { name, calories, fat, carbs, protein };
+function createData(name: string, value: string) {
+  return { name, value };
 }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+
 
 type SimpleTableProps = {
   mean: string,
@@ -36,22 +30,23 @@ type SimpleTableProps = {
 export default function SimpleTable({ mean, median, standardDeviation, mode }: SimpleTableProps) {
   const classes = useStyles();
 
+  const rows = [
+    createData('mean', mean),
+    createData('median', median),
+    createData('standard deviation', standardDeviation),
+    createData('mode', mode),
+  ];
+
   return (
     <TableContainer component={Paper}>
       <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>mean</TableCell>
-            <TableCell align="right">{mean}</TableCell>
-          </TableRow>
-        </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow key={row.name}>
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.value}</TableCell>
             </TableRow>
           ))}
         </TableBody>
